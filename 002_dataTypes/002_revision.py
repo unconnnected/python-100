241c4e46a8ac678f4de83223b27b68ac19bdc382
paulb <pabennett@hotmail.co.uk>
2023-12-03 01:05:15 +0000

    004 revision update

--- 002_dataTypes/002_revision.py
+++ 002_dataTypes/002_revision.py
@@ -2,6 +2,7 @@
 print("......................................................................")
 print("Basic Data Types")
 
+#Check data types
 if False:
     strg = input("What is your first name?\n")
     strglen = len(strg)
@@ -9,5 +10,25 @@ if False:
     #Check variable type
     print("str type: " + str(type(strg)))
     print("strlen type: " + str(type(strglen)))
+
+#Iterate through a string of numbers
+if False:
+    number = 134
+    numberString = str(number)
+
+    result = 0
+
+    for c in numberString:
+        result += int(c)
+    
+    print(result)
+
+#Including values within print
+if False:
+    name = "Paul"
+    age = 40
+
+    print(f"{name} will be {age} soon enough...\n")
+
 
 print("-End of script-")
