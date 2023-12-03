ad7c6ec9a63fc3ad6839751c34b51ddc1a8f968e
paulb <pabennett@hotmail.co.uk>
2023-12-03 00:48:37 +0000

    001 revision

---
+++ 001_variables/001_revision.py
@@ -0,0 +1,23 @@
+#Basic Variable Combining
+print("......................................................................")
+print("Basic Variable Combining")
+
+#Combine integers
+if False:
+    number1 = int(input("Enter the first number...\n"))
+    number2 = int(input("Enter the second number...\n"))
+
+    total = number1 + number2
+
+    print("The two numbers combined make " + str(total))
+
+#Combine strings
+if False:
+    string1 = input("Enter your first name...\n")
+    string2 = input("Enter your last name...\n")
+
+    combinedName = string1.capitalize() + " " + string2.capitalize()
+
+    print("Your name is: " + combinedName)
+
+print("-End of script-")
