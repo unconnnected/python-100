1221a66737de1c11c03203a55b63739ecad40f75
paulb <pabennett@hotmail.co.uk>
2023-12-03 00:56:15 +0000

    002 revision

---
+++ 002_dataTypes/002_revision.py
@@ -0,0 +1,13 @@
+#Basic Data Types
+print("......................................................................")
+print("Basic Data Types")
+
+if False:
+    strg = input("What is your first name?\n")
+    strglen = len(strg)
+
+    #Check variable type
+    print("str type: " + str(type(strg)))
+    print("strlen type: " + str(type(strglen)))
+
+print("-End of script-")
