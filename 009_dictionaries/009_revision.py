6b063893e5929644360bafd43c5f0ab02cfa6950
paulb <pabennett@hotmail.co.uk>
2023-12-07 05:03:57 +0000

    009 revision

---
+++ 009_dictionaries/009_revision.py
@@ -0,0 +1,19 @@
+#Basic Dictionaries
+print("......................................................................")
+print("Basic Dictionaries")
+
+#Basic dictionary
+if False:
+    aDict = {
+        "name": "Morgan",
+        "age": 86
+    }
+
+    #Adding to dictionary
+    aDict["Height"] = 1.88
+
+    #Iterating over a dictionary
+    for key in aDict:
+        print(f"{key}: {aDict[key]}")
+
+print("-End of script-")
