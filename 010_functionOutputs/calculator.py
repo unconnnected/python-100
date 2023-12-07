830294581c6a6cb26157b205b3961579807b02a4
paulb <pabennett@hotmail.co.uk>
2023-12-07 20:54:52 +0000

    update 010 calculator

--- 010_functionOutputs/calculator.py
+++ 010_functionOutputs/calculator.py
@@ -1,13 +1,11 @@
 #Calculator
-
-import os
 import logo
 
 
 #Input functions
 def inputNum(nth):
     numValid = False
-    num
+    num = 0
 
     while numValid == False:
         num = int(input(f"What is the {nth} number?\n"))
@@ -15,21 +13,21 @@ def inputNum(nth):
         if type(num) == int:
             numValid = True
         else:
-            print("Invalid number")
+            print("Invalid number, please try again")
 
     return num
 
 
 def inputOp():
     opValid = False
-    op
+    op = ""
 
     while opValid == False:
         print(f"+\n-\n*\n/")
         op = input("Pick an operation: \n")
 
         if(op != "+" and op != "-" and op != "/" and op != "*"):
-            print("Invalid operation")
+            print("Invalid operation, please try again")
         else:
             opValid = True
 
@@ -38,27 +36,30 @@ def inputOp():
 
 #Operator functions
 def add(a, b):
+    print(f"{a} + {b}")
     return a + b
 
 def sub(a, b):
+    print(f"{a} - {b}")
     return a - b
 
 def mult(a, b):
+    print(f"{a} * {b}")
     return a * b
 
 def div(a, b):
+    print(f"{a} / {b}")
     return a / b
 
 
-#initial
-result = 0
-print(logo.logo)
-print("")
-
-def mainCalc(useResult):
-    num1, num2, operator, r
+def mainCalc(useResult, result):
+    num1 = 0
+    num2 = 0
+    operator = ""
+    r = 0
 
     if useResult == True:
+        print(f"Continuing with {result}")
         num1 = result
     else:
         num1 = inputNum("first")
@@ -76,8 +77,26 @@ def mainCalc(useResult):
         case "/":
             r = div(num1, num2)
 
+    print(f"{r}")
+    input("Press Enter to continue...")
     return r
 
 
-def main():
+def main(continueCalc, result):
+    if(continueCalc == 'y'):
+        result = mainCalc(True, result)
+    else:
+        result = mainCalc(False, result)
+
+    return result
+
+
+continueCalc = ""
+result = 0
+while True:
+    print(logo.logo)
+    print("")
+    
+    result = main(continueCalc, result)
+
     continueCalc = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation:\n")
