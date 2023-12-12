3939d34ef86c4ca66eec894ee23ea1943dc26f26
paulb <pabennett@hotmail.co.uk>
2023-12-12 23:50:15 +0000

    leetcode 0350
    Intersection of two arrays ii

---
+++ _leetCode/0350-intersection-of-two-arrays-ii.py
@@ -0,0 +1,32 @@
+#Intersection Of Two Arrays II
+#https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+
+caseNums1_1 = [1,2,2,1]
+caseNums2_1 = [2,2]
+
+caseNums1_2 = [4,9,5]
+caseNums2_2 = [9,4,9,8,4]
+
+if True:
+    def intersectionOfTwoArraysII(nums1, nums2):
+        countDict = dict()
+        result = []
+
+        for number in nums1:
+            if number in countDict:
+                countDict[number] += 1
+            else:
+                countDict[number] = 1
+        
+        for number in nums2:
+            if number in countDict and countDict[number] > 0:
+                result.append(number)
+                countDict[number] -= 1
+
+                if countDict[number] == 0:
+                    countDict.pop(number)
+
+        print(f"{result}")
+
+    intersectionOfTwoArraysII(caseNums1_1, caseNums2_1)
+    intersectionOfTwoArraysII(caseNums1_2, caseNums2_2)
