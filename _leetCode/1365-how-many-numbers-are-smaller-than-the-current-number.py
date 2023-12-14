96075be34c8c8d1cf3489acd5509c770476e52a6
paulb <pabennett@hotmail.co.uk>
2023-12-14 12:53:25 +0000

    leetcode 1365
    How many numbers are smaller than the current number

---
+++ _leetCode/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -0,0 +1,21 @@
+#How Many Numbers Are Smaller Than The Current Number
+#https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/description/
+
+caseNums_1 = [8,1,2,2,3]
+caseNums_2 = [6,5,4,8]
+caseNums_3 = [7,7,7,7]
+
+if True:
+    def howManyNumbersAreSmallerThanTheCurrentNumber(nums):
+        ind = []
+        sortedNums = nums.copy()
+        sortedNums.sort()
+        
+        for num in nums:
+            ind.append(sortedNums.index(num))
+
+        print(f"{ind}")
+
+    howManyNumbersAreSmallerThanTheCurrentNumber(caseNums_1)
+    howManyNumbersAreSmallerThanTheCurrentNumber(caseNums_2)
+    howManyNumbersAreSmallerThanTheCurrentNumber(caseNums_3)
