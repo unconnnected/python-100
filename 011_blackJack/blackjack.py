20982c48ae1372cd13e5db8077df67f641654ded
paulb <pabennett@hotmail.co.uk>
2023-12-15 00:56:00 +0000

    update 011 blackjack
    Trying to add in the rule checks

--- 011_blackJack/blackjack.py
+++ 011_blackJack/blackjack.py
@@ -59,37 +59,76 @@ def generateInitialHand():
     playerCardHandValues.append(generateCard(True))
     dealerCardHandValues.append(generateCard(False))
 
-def askToHit():
+def askPlayerToHit():
     response = input("Would you like one more card? y/n")
 
     if response == 'y':
-        hitMe(True)
+        hit(True)
+        return True
+    else:
+        return False
+    
+def askDealerToHit():
+    while sum(dealerCardHandValues) > 21 and 11 in dealerCardHandValues:
+        dealerCardHandValues[dealerCardHandValues.index(11)] = 1
 
-def hitMe(playerCard):
+    if sum(dealerCardHandValues) >= 17:
+        return False
+    
+    #Dealer hits if able
+    hit(False)
+    return True
+
+def hit(playerCard):
     if playerCard == True:
         playerCardHandValues.append(generateCard(True))
     else:    
         dealerCardHandValues.append(generateCard(False))
 
-def checkOver21():
-    playerTotal = sum(playerCardHandValues)
+def checkOver21(cardHandValues):
+    total = sum(cardHandValues)
 
-    if playerTotal > 21 and 11 not in playerCardHandValues:
-        return True
+    while sum(cardHandValues) > 21 and 11 in cardHandValues:
+        cardHandValues[cardHandValues.index(11)] = 1
     
-    while sum(playerCardHandValues) > 21 and 11 in playerCardHandValues:
-        playerCardHandValues[playerCardHandValues.index(11)] = 1
-    
-    if playerTotal > 21:
+    if total > 21:
         return True
     
     return False
 
-def mainGame():
+def playBlackjack():
     generateInitialHand()
 
-        
+    #Check if player wants to get more cards
+    playerWantsCards = True
+    while playerWantsCards == True and checkOver21(playerCardHandValues) == False:
+        playerWantsCards = askPlayerToHit()
 
+    if checkOver21(playerCardHandValues) == True:
+        print(f"Your total is over 21, you lost...\n")
+        return True
+    else:
+        dealerWantsCards = True
+
+        while dealerWantsCards == True and checkOver21(dealerCardHandValues) == False:
+            dealerWantsCards = askDealerToHit()
+
+        #Compare hands
+        playerTotal = sum(playerCardHandValues)
+        dealerTotal = sum(dealerCardHandValues)
+
+        if playerTotal > 21:
+            print(f"Your total is over 21, you lost")
+        elif dealerTotal > 21 and playerTotal <= 21:
+            print(f"Dealer is over 21, you win")
+        elif playerTotal != 21 and dealerTotal == playerTotal:
+            print(f"You tied with the dealer")
+        elif playerTotal > dealerTotal:
+            print(f"You beat the dealer, you win")
+        elif dealerTotal > playerTotal:
+            print(f"The dealer beat you, you lost")
+
+        return True
 
 
 print(logo.logo)
