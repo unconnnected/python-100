f2242070a67e8b8172a01274068622c6f9ee540d
paulb <pabennett@hotmail.co.uk>
2023-12-18 07:04:22 +0000

    [LeetCode 0876]

---
+++ _leetCode/0876-middle-of-the-linked-list.py
@@ -0,0 +1,29 @@
+#Middle Of The Linked List
+#https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        last = head
+        middle = head
+
+        while last and last.next:
+            middle = middle.next
+            last = last.next.next
+        
+        return middle.val
+
+
+caseHead_1 = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+caseHead_2 = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6))))))
+
+solution = Solution()
+
+print(f"{solution.middleNode(caseHead_1)}")
+print(f"{solution.middleNode(caseHead_2)}")
