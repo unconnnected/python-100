7a34badabd7fd7c08aa31b8cd6f91b18c68fdaac
paulb <pabennett@hotmail.co.uk>
2023-12-19 00:11:52 +0000

    [013] Debug Leap Year

---
+++ 013_debugging/013_leapYear.py
@@ -0,0 +1,14 @@
+#Debugging Leap Year
+
+year = int(input("Which year do you want to check?\n"))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not a leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not a leap year")
