d7a05cf16b4b7f9fcc6a4b0a95aaf001a6e951a4
paulb <pabennett@hotmail.co.uk>
2023-12-19 00:06:52 +0000

    [013] Debug Odd Or Even
    Simple debugging exercise

---
+++ 013_debugging/013_oddOrEven.py
@@ -0,0 +1,8 @@
+#Debugging Odd Or Even
+
+number = int(input("Which number do you want to check?\n"))
+
+if number % 2 == 0:
+    print(f"This is an even number")
+else:
+    print(f"This is an odd number")
