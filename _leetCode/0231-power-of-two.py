81f965afda242f5f29043b27fffe1011e20f62a2
paulb <pabennett@hotmail.co.uk>
2023-12-20 18:49:06 +0000

    [LeetCode 0231]

---
+++ _leetCode/0231-power-of-two.py
@@ -0,0 +1,18 @@
+#Power Of Two
+#https://leetcode.com/problems/power-of-two/description/
+
+caseNum_1 = 1
+caseNum_2 = 16
+caseNum_3 = 3
+
+if True:
+    def isPowerOfTwo(num) -> bool:
+        result = 1
+        while num > result:
+            result *= 2
+        
+        return result == num
+    
+    print(f"{isPowerOfTwo(caseNum_1)}")
+    print(f"{isPowerOfTwo(caseNum_2)}")
+    print(f"{isPowerOfTwo(caseNum_3)}")
