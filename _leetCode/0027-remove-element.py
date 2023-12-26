1414c05b276c6620478e04dcb9650bcc4312952a
paulb <pabennett@hotmail.co.uk>
2023-12-26 08:47:01 +0000

    [LeetCode 0027] Fix
    Removed function calls in if statement

--- _leetCode/0027-remove-element.py
+++ _leetCode/0027-remove-element.py
@@ -1,24 +1,24 @@
 #Remove Element
 #https://leetcode.com/problems/remove-element/description/
 
-testCase1 = [3,2,2,3]
-testVal1 = 3
+caseNums_1 = [3,2,2,3]
+caseVal_1 = 3
 
-testCase2 = [0,1,2,2,3,0,4,2]
-testVal2 = 2
+caseNums_2 = [0,1,2,2,3,0,4,2]
+caseVal_2 = 2
 
 if True:
-    def removeElement(case_, val_):
+    def removeElement(nums, val) -> int:
         i = 0
         
-        while i < len(case_):
-            if case_[i] == val_:
-                case_.pop(i)
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
                 continue
             
             i += 1
 
-        print(f"{case_}")
+        return i
 
-    removeElement(testCase1, testVal1)
-    removeElement(testCase2, testVal2)
+print(f"{removeElement(caseNums_1, caseVal_1)}")
+print(f"{removeElement(caseNums_2, caseVal_2)}")
