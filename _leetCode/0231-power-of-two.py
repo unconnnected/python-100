c08ff4f299066699c0a18e04bc61310e0395de56
paulb <pabennett@hotmail.co.uk>
2023-12-26 15:06:25 +0000

    [LeetCode 0231] +
    Solution dividing the num down

--- _leetCode/0231-power-of-two.py
+++ _leetCode/0231-power-of-two.py
@@ -5,14 +5,23 @@ caseNum_1 = 1
 caseNum_2 = 16
 caseNum_3 = 3
 
-if True:
+#Multiplying up
+if False:
     def isPowerOfTwo(num) -> bool:
         result = 1
         while num > result:
             result *= 2
         
         return result == num
-    
+
+#Dividing down
+if True:
+    def isPowerOfTwo(num) -> bool:
+        while num % 2 == 0:
+            num /= 2
+
+        return num == 1
+
 print(f"{isPowerOfTwo(caseNum_1)}")
 print(f"{isPowerOfTwo(caseNum_2)}")
 print(f"{isPowerOfTwo(caseNum_3)}")
