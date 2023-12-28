f928ad68555f9843da181f66eaf86d6ce8f7c9c3
paulb <pabennett@hotmail.co.uk>
2023-12-28 17:29:13 +0000

    [LeetCode 0268] Fix
    Updated function to return i
    Removed function calls in if statement

--- _leetCode/0268-missing-number.py
+++ _leetCode/0268-missing-number.py
@@ -5,16 +5,15 @@ caseNums_1 = [3,0,1]
 caseNums_2 = [0,1]
 caseNums_3 = [9,6,4,2,3,5,7,0,1]
 
-
 if True:
     def missingNumber(nums):
-        nums_ = set(nums)
+        numsSet = set(nums)
 
         for i in range(len(nums)):
-            if i not in (nums_):
-                print(f"{i}")
+            if i not in (numsSet):
+                return i
                 break
-    
-    missingNumber(caseNums_1)
-    missingNumber(caseNums_2)
-    missingNumber(caseNums_3)
+
+print(f"{missingNumber(caseNums_1)}")
+print(f"{missingNumber(caseNums_2)}")
+print(f"{missingNumber(caseNums_3)}")
