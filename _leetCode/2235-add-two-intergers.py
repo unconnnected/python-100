18def62f6db794c2c2a85a4493503b73973bd328
paulb <pabennett@hotmail.co.uk>
2024-01-01 20:22:06 +0000

    [LeetCode 2235]

---
+++ _leetCode/2235-add-two-intergers.py
@@ -0,0 +1,15 @@
+#Add Two Integers
+#https://leetcode.com/problems/add-two-integers/description/
+
+caseNum1_1 = 12
+caseNum2_1 = 5
+
+caseNum1_2 = -10
+caseNum2_2 = 4
+
+if True:
+    def addTwoIntegers(num1, num2) -> int:
+        return num1 + num2
+
+print(f"{addTwoIntegers(caseNum1_1, caseNum2_1)}")
+print(f"{addTwoIntegers(caseNum1_2, caseNum2_2)}")
