d5d53f158b00928c9320691f1854f78d3213c22d
paulb <pabennett@hotmail.co.uk>
2024-01-03 06:36:35 +0000

    [LeetCode 2894] +
    Added original solution

--- _leetCode/2894-divisible-and-non-divisible-sums-difference.py
+++ _leetCode/2894-divisible-and-non-divisible-sums-difference.py
@@ -10,6 +10,19 @@ caseM_2 = 6
 caseN_3 = 5
 caseM_3 = 1
 
+if False:
+    def differenceOfSums(n, m) -> int:
+        num1 = 0
+        num2 = 0
+
+        for i in range(1, n + 1):
+            if i % m == 0:
+                num2 += i
+            else:
+                num1 += i
+
+        return num1 - num2
+
 if True:
     def differenceOfSums(n, m) -> int:
         sum = 0
