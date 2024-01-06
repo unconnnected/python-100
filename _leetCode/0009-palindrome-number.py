016ccd96dc3eb12cf06a4de7647035ffaf61f6ec
paulb <pabennett@hotmail.co.uk>
2024-01-06 10:08:58 +0000

    [LeetCode 0009] +
    Added shortened solution

--- _leetCode/0009-palindrome-number.py
+++ _leetCode/0009-palindrome-number.py
@@ -5,12 +5,17 @@ caseNum_1 = 121
 caseNum_2 = -121
 caseNum_3 = 10
 
-if True:
+if False:
     def isPalindrome(num):
         numString = str(num)
         numStringReversed = numString[::-1]
 
         return numString == numStringReversed
+    
+if True:
+    def isPalindrome(n):
+        ns = str(n)
+        return ns == ns[::-1]
 
 print(f"{isPalindrome(caseNum_1)}")
 print(f"{isPalindrome(caseNum_2)}")
